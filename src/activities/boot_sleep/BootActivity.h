ee7918f32840ed4af100475be7b8c4ed153bddd6
einklover <9z2g8ywfbf@privaterelay.appleid.com>
2026-08-08 11:08:51 +0800

    fix: keep boot screen free of touch chrome

--- src/activities/boot_sleep/BootActivity.h
+++ src/activities/boot_sleep/BootActivity.h
@@ -6,4 +6,5 @@ class BootActivity final : public Activity {
   explicit BootActivity(GfxRenderer& renderer, MappedInputManager& mappedInput)
       : Activity("Boot", renderer, mappedInput) {}
   void onEnter() override;
+  bool showTouchNavigation() const override { return false; }
 };
